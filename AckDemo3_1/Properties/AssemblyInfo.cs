0718c0fce99225b515e1453cf701998d314ebca8
Jan Staelens <jan.staelens@skyline.be>
2022-12-09 11:50:56 +0100

    Add project files.

---
+++ AckDemo3_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AckDemo3_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AckDemo3_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("855E5FCB-118B-4542-8BD3-EA535B5975E6")]
+[assembly: AssemblyVersion("1.0.0.0")]
